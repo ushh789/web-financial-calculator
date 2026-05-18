ec36b4f15540604b06d60e4a255ff16033c596aa
Yurii Flys <flysyurii@gmail.com>
2026-05-18 23:31:41 +0300

    chore(storybook): add msw addon and mock next-intl/server for RSC support

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -1,20 +1,32 @@
-import type { StorybookConfig } from '@storybook/nextjs-vite';
+import path from "path";
+import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-onboarding",
+    "msw-storybook-addon",
   ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "..\\public"
-  ]
+  framework: "@storybook/nextjs-vite",
+  staticDirs: ["../public"],
+  viteFinal: async (config) => {
+    config.resolve ??= {};
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "next-intl/server": path.resolve(
+        __dirname,
+        "./mocks/next-intl-server.ts"
+      ),
+    };
+    return config;
+  },
 };
+
 export default config;
